a0ed9b35f201b09d7933184d2d2e06b89ecbb2a8
Karlo Verde <karlo.verde.1996@gmail.com>
2022-08-20 16:22:07 -0500

    chore: load connection string from env

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,18 @@
-interface Config { }
+interface Config {
+  db: DbConfig;
+}
 
-const config: Config = {};
+interface DbConfig {
+  connectionString: string;
+}
+
+const connectionString = process.env.DB_CONNECTION_STRING;
+if (!connectionString) throw new Error('DB_CONNECTION_STRING environment variable is not set');
+
+const config: Config = {
+  db: {
+    connectionString,
+  },
+};
 
 export default config;
