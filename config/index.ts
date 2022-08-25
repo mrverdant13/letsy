f818835fe246a6bde3919780cd279d0d73c9dcb1
Karlo Verde <karlo.verde.1996@gmail.com>
2022-08-25 01:57:04 -0500

    chore(auth): load GH OAuth secrets

--- config/index.ts
+++ config/index.ts
@@ -1,17 +1,35 @@
 interface Config {
   db: DbConfig;
+  githubOauthApp: GithubOauthAppConfig;
 }
 
 interface DbConfig {
   connectionString: string;
 }
 
-const connectionString = process.env.DB_CONNECTION_STRING;
+interface GithubOauthAppConfig {
+  clientId: string;
+  clientSecret: string;
+}
+
+const env = process.env;
+
+const connectionString = env.DB_CONNECTION_STRING;
 if (!connectionString) throw new Error('DB_CONNECTION_STRING environment variable is not set');
+
+const githubOauthAppClientId = env.GITHUB_OAUTH_APP_CLIENT_ID;
+if (!githubOauthAppClientId) throw new Error('GITHUB_OAUTH_APP_CLIENT_ID environment variable is not set');
+
+const githubOauthAppClientSecret = env.GITHUB_OAUTH_APP_CLIENT_SECRET;
+if (!githubOauthAppClientSecret) throw new Error('GITHUB_OAUTH_APP_CLIENT_SECRET environment variable is not set');
 
 const config: Config = {
   db: {
     connectionString,
+  },
+  githubOauthApp: {
+    clientId: githubOauthAppClientId,
+    clientSecret: githubOauthAppClientSecret,
   },
 };
 
