98ed9c097252ab4eb95a497ba08a256bb0f0d480
Karlo Verde <karlo.verde.1996@gmail.com>
2022-08-25 01:57:32 -0500

    refactor(db): return current Mongo client

--- database/db.ts
+++ database/db.ts
@@ -1,31 +1,29 @@
+import type { MongoClient } from 'mongodb';
 import mongoose from 'mongoose';
 
 import config from '../config';
 
 let isConnected: boolean;
 
-export const connect = async () => {
-  try {
-    const activeConnection: mongoose.Connection | undefined = mongoose.connections.find(
-      // `readyState` has a value of `1` when the connection is open.
-      (c) => (c.readyState === 1),
-    );
-    if (activeConnection) {
-      console.log('Already connected to database');
-      return;
-    }
-    const connectionString = config.db.connectionString;
-    await mongoose.disconnect();
-    await mongoose.connect(connectionString);
-    isConnected = true;
-    console.log('Connected to database:', connectionString);
-  } catch (e) {
-    console.error('Error connecting to database:', e);
-    isConnected = false;
+export const connect = async (): Promise<MongoClient> => {
+  const activeConnection: mongoose.Connection | undefined = mongoose.connections.find(
+    // `readyState` has a value of `1` when the connection is open.
+    (c) => (c.readyState === 1),
+  );
+  if (activeConnection) {
+    console.log('Already connected to database');
+    return activeConnection.getClient() as MongoClient;
   }
+  const connectionString: string = config.db.connectionString;
+  await mongoose.disconnect();
+  const m: typeof mongoose = await mongoose.connect(connectionString);
+  const client: MongoClient = m.connection.getClient() as MongoClient;
+  isConnected = true;
+  console.log('Connected to database:', connectionString);
+  return client;
 }
 
-export const disconnect = async () => {
+export const disconnect = async (): Promise<void> => {
   if (!isConnected) {
     console.log('Already disconnected from database');
     return;
