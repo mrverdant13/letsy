1af88323297e3025bfbb3bb0eef60cf16f8dd126
Karlo Verde <karlo.verde.1996@gmail.com>
2022-08-28 12:56:38 -0500

    chore(auth): inject session data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,16 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { darkTheme } from '../themes';
 
 import '../styles/globals.css'
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
 
