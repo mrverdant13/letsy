d9ef99a3e3761c17c3f3288e5945b87762ee3438
Karlo Verde <karlo.verde.1996@gmail.com>
2022-09-01 21:12:24 -0500

    chore: create tools button

---
+++ components/ui/ToolsButton.tsx
@@ -0,0 +1,86 @@
+import Link from 'next/link'
+import { useState } from 'react';
+
+import { Handyman } from '@mui/icons-material'
+import { IconButton, Tooltip, Button, Menu, MenuItem } from '@mui/material';
+
+interface ToolData {
+  route: string;
+  name: string;
+}
+
+const tools: ToolData[] = [
+  {
+    route: '/tools/equivalent-value',
+    name: 'Equivalent Value Calculator',
+  },
+];
+
+export const ToolsButton = () => {
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+
+  const open = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const close = () => {
+    setAnchorEl(null);
+  };
+
+  const isOpen: boolean = Boolean(anchorEl);
+  const menuId: string = 'tools-menu';
+
+  return (
+    <>
+      <Button
+        aria-controls={isOpen ? menuId : undefined}
+        aria-haspopup="true"
+        aria-expanded={isOpen ? 'true' : undefined}
+        onClick={open}
+        sx={{
+          mx: 3,
+        }}
+      >
+        Tools
+      </Button>
+      <Menu
+        id={menuId}
+        open={isOpen}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        onClose={close}
+      >
+        {tools.map((tool, index) => (
+          <Link
+            key={index}
+            href={tool.route}
+            passHref
+          >
+            <MenuItem
+              sx={{
+                px: 3,
+              }}
+            >
+              {tool.name}
+            </MenuItem>
+          </Link>
+        ))}
+      </Menu>
+    </>
+  );
+
+  return (
+    <Tooltip title="Tools">
+      <IconButton aria-label="Go to tools page">
+        <Handyman />
+      </IconButton>
+    </Tooltip>
+  )
+}
