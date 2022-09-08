7828cf21a50760e5d380a593f2511497f61049ca
Karlo Verde <karlo.verde.1996@gmail.com>
2022-09-07 19:23:54 -0500

    feat(equivalence): expose computation flow

--- context/equivalent-value/EquivalentValueProvider.tsx
+++ context/equivalent-value/EquivalentValueProvider.tsx
@@ -1,18 +1,22 @@
 import { FC, Reducer, useReducer } from "react";
 
-import { EquivalentValueAction, EquivalentValueState, reducer } from './equivalentValueReducer';
+import { EquivalentValueState, reducer } from './equivalentValueReducer';
 import { EquivalentValueContext } from './EquivalentValueContext';
-import { IPayment } from "../../interfaces/payment";
+import { IPayment, ISinglePayment } from '../../interfaces/payment';
+import { IPaymentType } from "../../interfaces/payment-type";
+import httpClient from '../../apis/_client';
+import { EquivalentValueAction } from "./actions";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const INITIAL_STATE: EquivalentValueState = {
-  loading: false,
+  computing: false,
   group: {
     name: 'Untitled',
-    payments: [],
+    payments: [
+    ],
   },
 };
 
@@ -37,11 +41,32 @@ export const EquivalentValueProvider: FC<Props> = ({ children }) => {
     });
   }
 
+  const computeEquivalentValue = async (
+    targetPeriod: number,
+  ): Promise<void> => {
+    dispatch({
+      type: "[EquivalentValue] Computing Equivalent Payment",
+    });
+    const response = await httpClient.post<ISinglePayment>(
+      '/equivalent-value',
+      {
+        group: state.group,
+        targetPosition: targetPeriod,
+      },
+    );
+    const equivalentPayment: ISinglePayment = response.data;
+    dispatch({
+      type: "[EquivalentValue] Computed Equivalent Payment",
+      equivalentPayment,
+    });
+  }
+
   return (
     <EquivalentValueContext.Provider value={{
       ...state,
       updatePayment,
       setInterest,
+      computeEquivalentValue,
     }}>
       {children}
     </EquivalentValueContext.Provider>
