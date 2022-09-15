cf7629fe72f520ecae21cd7c510fd60e16c81feb
Karlo Verde <karlo.verde.1996@gmail.com>
2022-09-15 01:52:42 -0500

    chore(payment-group): create payment group DB model

---
+++ database/models/payment-group.ts
@@ -0,0 +1,46 @@
+import { prop, modelOptions, getModelForClass } from '@typegoose/typegoose';
+import { IPayment } from '../../interfaces/payment';
+
+import { Payment, paymentDiscriminators } from './payment';
+import { GenericPaymentGroupPaymentsValidationSchema } from '../../validation-schemas/payment-group';
+
+const uniquePaymentNamesValidator = {
+  validator: (v: Payment[]) => {
+    const result = GenericPaymentGroupPaymentsValidationSchema.safeParse(v);
+    return result.success;
+  },
+  message: 'A payment group must hold payments with unique names',
+};
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'payment-groups',
+  },
+})
+export class PaymentGroup {
+  @prop({
+    required: true,
+    minlength: 1,
+    trim: true,
+  })
+  name!: string;
+
+  @prop({
+    required: true,
+    type: Payment,
+    discriminators: () => paymentDiscriminators,
+    default: [],
+    validate: [
+      uniquePaymentNamesValidator,
+    ],
+  })
+  payments!: IPayment[];
+
+  @prop({
+    required: true,
+    min: 0,
+  })
+  interest!: number;
+}
+
+export const PaymentGroupModel = getModelForClass(PaymentGroup);
