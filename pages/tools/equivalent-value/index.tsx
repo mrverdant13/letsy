8ff4f33f28d2c6fc1a2159f22f1809cd051eac81
Karlo Verde <karlo.verde.1996@gmail.com>
2022-09-15 23:30:39 -0500

    feat(groups): allow group creation

--- pages/tools/equivalent-value/index.tsx
+++ pages/tools/equivalent-value/index.tsx
@@ -1,29 +1,23 @@
 import { NextPage, GetServerSideProps, GetServerSidePropsContext } from 'next';
 import React, { FC, useState, useMemo } from 'react';
 
-import { Stack, Box, Typography } from '@mui/material';
 import { unstable_getServerSession } from 'next-auth';
 
 import { BasePageLayout } from '../../../components/layouts/BasePageLayout';
-import { EquivalentValueProvider } from '../../../context/equivalent-value/EquivalentValueProvider';
-import { useEquivalentValueContext } from '../../../context/equivalent-value/EquivalentValueContext';
-import { UniformSeriesPayment } from '../../../components/ui/UniformSeriesPayment';
-import { IPaymentType } from '../../../interfaces/payment-type';
-import { SinglePayment } from '../../../components/ui/SinglePayment';
-import { PeriodsBar } from '../../../components/ui/PeriodsBar';
-import { EquivalenceToolbar } from '../../../components/ui/EquivalenceToolbar';
-import { AddPaymentButton } from '../../../components/ui/AddPaymentButton';
 import { authOptions } from '../../api/auth/[...nextauth]';
+import { AddPaymentGroupButton } from '../../../components/ui/AddPaymentGroupButton';
+import { EquivalentValueGroupsProvider } from '../../../context/equivalent-value-groups/EquivalentValueGroupsProvider';
 
 const EquivalentValueGroupsPage: NextPage = () => {
   return (
-    <EquivalentValueProvider>
+    <EquivalentValueGroupsProvider>
       <BasePageLayout
         title="Equivalent Value Calculation - Groups"
         description="Payment groups"
       >
+        <AddPaymentGroupButton />
       </BasePageLayout>
-    </EquivalentValueProvider>
+    </EquivalentValueGroupsProvider>
   );
 }
 export default EquivalentValueGroupsPage;
