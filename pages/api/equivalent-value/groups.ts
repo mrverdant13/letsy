6db778b1eacc98cb0d4667bd19ea19bbdc9cade5
Karlo Verde <karlo.verde.1996@gmail.com>
2022-09-15 19:34:33 -0500

    refactor(equivalence): relocate handler impl

--- pages/api/equivalent-value/groups.ts
+++ pages/api/equivalent-value/groups.ts
@@ -6,9 +6,10 @@ import { MethodNotAllowedResponse } from '../../../interfaces/api/method-not-all
 import { NewPaymentGroupValidationSchema } from '../../../validation-schemas/payment-group';
 import { INewPaymentGroup, IPaymentGroup } from '../../../interfaces/payment-group';
 import { BadRequestWithInvalidDataResponse } from '../../../interfaces/api/bad-request-response';
-import { createEquivalentValuePaymentGroup } from '../../../services/equivalent-value.service';
 import { authOptions } from '../auth/[...nextauth]';
 import { UnauthorizedResponse } from '../../../interfaces/api/unauthorized-response';
+import { db } from '../../../database';
+import { PaymentGroupModel } from '../../../database/models/payment-group';
 
 type CreateEquivalentValueGroup =
   | UnauthorizedResponse
@@ -24,12 +25,12 @@ type Data =
 export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
   switch (req.method) {
     case 'POST':
-      return post(req, res);
+      return createGroup(req, res);
   }
   return res.status(405).json({ message: 'Method Not Allowed' });
 }
 
-const post = async (req: NextApiRequest, res: NextApiResponse<CreateEquivalentValueGroup>) => {
+const createGroup = async (req: NextApiRequest, res: NextApiResponse<CreateEquivalentValueGroup>) => {
   const session = await unstable_getServerSession(req, res, authOptions);
   if (!session) return res.status(401).send({ message: 'Unauthorized' });
   const result = NewPaymentGroupValidationSchema.safeParse({ ...req.body, owner: session.userId });
@@ -41,6 +42,7 @@ const post = async (req: NextApiRequest, res: NextApiResponse<CreateEquivalentVa
     });
   }
   const paymentGroup: INewPaymentGroup = result.data;
-  const newPaymentGroup = await createEquivalentValuePaymentGroup(paymentGroup);
-  return res.status(201).json(newPaymentGroup);
+  await db.connect();
+  const newPaymentGroup = await PaymentGroupModel.create(paymentGroup);
+  return res.status(201).json(newPaymentGroup.toJSON());
 }
