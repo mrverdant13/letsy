8b0f8f316aae009e8648ac7b88e10947287132a9
Karlo Verde <karlo.verde.1996@gmail.com>
2022-09-16 02:33:44 -0500

    chore(groups): create paginated getter endpoint

--- pages/api/equivalent-value/groups.ts
+++ pages/api/equivalent-value/groups.ts
@@ -4,7 +4,7 @@ import { unstable_getServerSession } from 'next-auth';
 
 import { MethodNotAllowedResponse } from '../../../interfaces/api/method-not-allowed-response';
 import { NewPaymentGroupValidationSchema } from '../../../validation-schemas/payment-group';
-import { INewPaymentGroup, IPaymentGroup } from '../../../interfaces/payment-group';
+import { IPaymentGroup, IPaymentGroupsPage } from '../../../interfaces/payment-group';
 import { BadRequestWithInvalidDataResponse } from '../../../interfaces/api/bad-request-response';
 import { authOptions } from '../auth/[...nextauth]';
 import { UnauthorizedResponse } from '../../../interfaces/api/unauthorized-response';
@@ -17,15 +17,23 @@ type CreateEquivalentValueGroup =
   | IPaymentGroup
   ;
 
+type GetEquivalentValueGroups =
+  | UnauthorizedResponse
+  | IPaymentGroupsPage
+  ;
+
 type Data =
   | MethodNotAllowedResponse
   | CreateEquivalentValueGroup
+  | GetEquivalentValueGroups
   ;
 
 export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
   switch (req.method) {
     case 'POST':
       return createGroup(req, res);
+    case 'GET':
+      return getGroups(req, res);
   }
   return res.status(405).json({ message: 'Method Not Allowed' });
 }
@@ -48,4 +56,24 @@ const createGroup = async (req: NextApiRequest, res: NextApiResponse<CreateEquiv
   await db.connect();
   const newPaymentGroup = await PaymentGroupModel.create(paymentGroup);
   return res.status(201).json(newPaymentGroup.toJSON());
+}
+
+const getGroups = async (req: NextApiRequest, res: NextApiResponse<GetEquivalentValueGroups>) => {
+  const session = await unstable_getServerSession(req, res, authOptions);
+  if (!session) return res.status(401).send({ message: 'Unauthorized' });
+  const filter = { owner: session.userId };
+  const query = req.query;
+  const offset = query.offset ? parseInt(`${query.offset}`) : 0;
+  const limit = query.limit ? parseInt(`${query.limit}`) : 25;
+  if (isNaN(offset) || isNaN(limit) || offset < 0 || limit < 1) {
+    return res.status(400).json({ message: 'Invalid pagination parameters' });
+  }
+  const [groups, groupsCount] = await Promise.all([
+    PaymentGroupModel.find(filter).skip(offset).limit(limit),
+    PaymentGroupModel.countDocuments(filter),
+  ]);
+  return res.status(200).json({
+    groups: groups.map(g => g.toJSON()),
+    count: groupsCount,
+  });
 }
