454484897803c156ef873d7c9f74b7ae75eb00c5
Karlo Verde <karlo.verde.1996@gmail.com>
2022-09-16 20:10:17 -0500

    fix: drop test component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,10 @@
 import type { NextPage } from 'next'
 
-import { Typography, Button } from '@mui/material';
+import { Typography } from '@mui/material';
 
 import { BasePageLayout } from '../components/layouts';
-import httpClient from '../apis/_client';
 
 const HomePage: NextPage = () => {
-  const onClick = () => {
-    httpClient.post(
-      '/equivalent-value/groups',
-      {
-        name: "A payment group",
-        interest: 1.2,
-        payments: [
-          {
-            type: "single",
-            name: "A single payment",
-            position: 0,
-            amount: 1.0,
-          },
-        ],
-      },
-    );
-  };
   return (
     <BasePageLayout>
       <Typography
@@ -31,11 +13,6 @@ const HomePage: NextPage = () => {
       >
         Welcome to Letsy!
       </Typography>
-      <Button
-        onClick={onClick}
-      >
-        TEST
-      </Button>
     </BasePageLayout >
   )
 }
