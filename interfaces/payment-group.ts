5cabde5fdd6ed02c16b35f900f472a5b4a30453d
Karlo Verde <karlo.verde.1996@gmail.com>
2022-09-17 03:06:14 -0500

    chore: remove unused elements

--- interfaces/payment-group.ts
+++ interfaces/payment-group.ts
@@ -1,11 +1,9 @@
 import { z } from 'zod';
 
-import { PaymentGroupValidationSchema, PaymentGroupWithOptionalInterestValidationSchema, NewPaymentGroupValidationSchema } from '../validation-schemas/payment-group';
+import { PaymentGroupValidationSchema, NewPaymentGroupValidationSchema } from '../validation-schemas/payment-group';
 
 export type IPaymentGroup = z.infer<typeof PaymentGroupValidationSchema>;
 export type INewPaymentGroup = z.infer<typeof NewPaymentGroupValidationSchema>;
-
-export type IPaymentGroupWithOptionalInterest = z.infer<typeof PaymentGroupWithOptionalInterestValidationSchema>;
 
 export type IPaymentGroupsPage = {
   groups: IPaymentGroup[];
