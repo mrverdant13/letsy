5992f847a974dcbe7c62e279bf210301d3a6c2fa
Karlo Verde <karlo.verde.1996@gmail.com>
2022-09-18 02:37:22 -0500

    chore(equivalence): equivalence computation

--- context/equivalence/EquivalenceProvider.tsx
+++ context/equivalence/EquivalenceProvider.tsx
@@ -4,9 +4,12 @@ import { EquivalenceState } from './state';
 import { EquivalenceAction } from './actions';
 import { reducer } from './reducer';
 import { EquivalenceContext } from './context';
+import httpClient from '../../apis/_client';
+import { ISinglePayment } from '../../interfaces/payment';
+import { IPaymentGroup } from '../../interfaces/payment-group';
 
 interface Props {
-  children: ReactNode[];
+  children: ReactNode[] | ReactNode;
 }
 
 export const EquivalenceProvider: FC<Props> = ({ children }) => {
@@ -17,10 +20,40 @@ export const EquivalenceProvider: FC<Props> = ({ children }) => {
     },
   );
 
+  const computeEquivalence = async (
+    group: IPaymentGroup,
+    targetPeriod: number,
+  ): Promise<void> => {
+    dispatch({
+      type: "[Equivalence] Loading",
+    });
+    try {
+      const response = await httpClient.post<ISinglePayment>(
+        '/equivalent-value',
+        {
+          group,
+          targetPosition: targetPeriod,
+        },
+      );
+      const equivalentPayment: ISinglePayment = response.data;
+      dispatch({
+        type: "[Equivalence] Loaded",
+        equivalentPayment,
+      });
+    } catch (e) {
+      dispatch({
+        type: "[Equivalence] Add Error",
+        error: { code: 'unexpected' },
+      });
+    }
+  }
+
+
   return (
     <EquivalenceContext.Provider
       value={{
         ...state,
+        computeEquivalence,
       }}
     >
     </EquivalenceContext.Provider>
--- context/equivalence/context.ts
+++ context/equivalence/context.ts
@@ -1,9 +1,13 @@
 import { createContext, useContext } from 'react';
 
 import { EquivalenceState } from './state';
+import { IPaymentGroup } from '../../interfaces/payment-group';
 
 type CtxProps =
   & EquivalenceState
+  & {
+    computeEquivalence: (group: IPaymentGroup, targetPosition: number) => Promise<void>;
+  }
   ;
 
 export const EquivalenceContext = createContext<CtxProps>({} as CtxProps);
