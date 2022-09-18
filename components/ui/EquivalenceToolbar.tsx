1f01c05da8996de4ec74b88cae249b63f43202c0
Karlo Verde <karlo.verde.1996@gmail.com>
2022-09-17 19:50:05 -0500

    feat(eq-group): display interest field

--- components/ui/EquivalenceToolbar.tsx
+++ components/ui/EquivalenceToolbar.tsx
@@ -1,45 +1,12 @@
-import { ChangeEvent, useEffect, useMemo, useState } from 'react';
-
-import { Paper, Stack, Typography, TextField, InputAdornment, Divider } from '@mui/material';
-import { CheckCircleOutline } from '@mui/icons-material';
+import { Paper, Stack, Typography, Divider } from '@mui/material';
 
 import { useEquivalentValueContext } from '../../context/equivalent-value/EquivalentValueContext';
-import useDebounce from '../../hooks/useDebounce';
 import { useEquivalenceGroupContext } from '../../context/equivalence-group/context';
-
+import { InterestField } from './InterestField';
 
 export const EquivalenceToolbar = () => {
-  const { group: { interest } } = useEquivalenceGroupContext();
-  const { computing, computeEquivalentValueError, equivalentPayment, setInterest } = useEquivalentValueContext();
-  const [dynamicInterest, setDynamicInterest] = useState(interest);
-  const [value, setValue] = useState<string>('');
-  const [isInvalidInterest, setIsInvalidInterest] = useState<boolean>(false);
-  const debouncedInterest = useDebounce(dynamicInterest, 500);
-  const isSaved = useMemo(
-    () => {
-      return debouncedInterest == interest;
-    },
-    [debouncedInterest, interest],
-  );
-
-  useEffect(
-    () => {
-      if (debouncedInterest == undefined) return;
-      setInterest(debouncedInterest);
-    },
-    [debouncedInterest],
-  );
-
-  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const value: string = event.target.value.trim();
-    setValue(value);
-    const interest: number = Number(+value);
-    const isInvalidInterest: boolean = isNaN(interest) || value.length === 0;
-    setIsInvalidInterest(isInvalidInterest);
-    if (isInvalidInterest) return;
-    setDynamicInterest(interest);
-  };
-
+  const { loading } = useEquivalenceGroupContext();
+  const { computeEquivalentValueError, equivalentPayment } = useEquivalentValueContext();
 
   return (
     <Paper
@@ -54,35 +21,10 @@ export const EquivalenceToolbar = () => {
         alignItems="center"
         spacing={2}
       >
-        <Stack
-          direction="row"
-          alignItems="center"
-          spacing={1}
-        >
-          <Typography variant="body1">
-            Interest:
-          </Typography>
-          <TextField
-            value={value}
-            onChange={onChange}
-            error={isInvalidInterest}
-            variant="outlined"
-            size="small"
-            sx={{
-              width: '15ch',
-            }}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  %
-                </InputAdornment>
-              ),
-            }}
-          />
-        </Stack>
+        <InterestField />
         <Divider orientation="vertical" flexItem />
         {
-          computing &&
+          loading &&
           <Typography>
             Loading...
           </Typography>
@@ -94,37 +36,13 @@ export const EquivalenceToolbar = () => {
           </Typography>
         }
         {
-          !computing &&
+          !loading &&
           !computeEquivalentValueError &&
           equivalentPayment &&
           <Typography>
             Amount: {equivalentPayment.amount} - Period: {equivalentPayment.position}
           </Typography>
         }
-        {/* <Stack
-          direction="row"
-          alignItems="center"
-          spacing={0.5}
-        >
-          {
-            value.length > 0 &&
-            !isInvalidInterest &&
-            (
-              isSaved
-                ? <>
-                  <Typography variant="body2">
-                    Saved
-                  </Typography>
-                  <CheckCircleOutline fontSize="small" />
-                </>
-                : <>
-                  <Typography variant="body2">
-                    Saving...
-                  </Typography>
-                </>
-            )
-          }
-        </Stack> */}
       </Stack>
     </Paper >
   )
