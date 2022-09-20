b6474d0819b8cba19e8bc77511417526466e7ee1
Karlo Verde <42245236+mrverdant13@users.noreply.github.com>
2022-09-19 22:45:59 -0500

    fix(eq-interest): use proper validation (#30)

--- pages/tools/equivalent-interest-rate/index.tsx
+++ pages/tools/equivalent-interest-rate/index.tsx
@@ -1,11 +1,11 @@
 import { NextPage } from 'next';
-import { ChangeEvent, FC, Ref, useEffect, useRef, useState } from 'react';
+import { FC, Ref, useEffect, useRef, useState } from 'react';
 
 import { z } from 'zod';
 import { Percent } from '@mui/icons-material';
-import { Box, Card, Divider, FilledInputProps, InputAdornment, Stack, TextField, Typography } from '@mui/material';
-import { Equation, EquationEvaluate, EquationOptions, defaultErrorHandler } from "react-equation";
-import { defaultVariables, defaultFunctions } from "equation-resolver";
+import { Box, Card, FilledInputProps, InputAdornment, Stack, TextField, Typography } from '@mui/material';
+import { Equation, EquationEvaluate } from "react-equation";
+import { Field, FieldProps, Form, Formik, useFormikContext } from 'formik';
 
 import { BasePageLayout } from '../../../components/layouts';
 import { InterestValidationSchema } from '../../../validation-schemas/interests';
@@ -40,62 +40,47 @@ const EquivalentInterestRatePage: NextPage = () => {
 export default EquivalentInterestRatePage;
 
 const digits: number = 4;
+const desiredInterestToken = 'i_d';
+const knownInterestToken = 'i_k';
+const desiredCompoundingPeriodsToken = 'p_d';
+const knownCompoundingPeriodsToken = 'p_k';
 
-const CompoundingPeriodsValidationSchema = z
-  .number()
-  .int()
-  .positive();
+const buildEquation = (
+  knownInterest: string | number,
+  knownCompoundingPeriods: string | number,
+  desiredCompoundingPeriods: string | number,
+) => {
+  return `(1 + ${knownInterest}) ^ (${knownCompoundingPeriods} / ${desiredCompoundingPeriods}) - 1`;
+}
 
-const EquivalentInterestRatePageContent = () => {
-  const equationRef = useRef<any>();
-
-  const [knownInterest, setKnownInterest] = useState<number>(0);
-  const [knownCompoundingPeriods, setKnownCompoundingPeriods] = useState<number>(0);
-  const [desiredCompoundingPeriods, setDesiredCompoundingPeriods] = useState<number>(0);
-  const [desiredInterest, setDesiredInterest] = useState<number>(0);
-
-  useEffect(
-    () => {
-      setDesiredInterest(
-        equationRef.current == null
-          ? 0
-          : equationRef.current.result.value,
-      );
-    },
+const CompoundingPeriodsValidationSchema = (name: string) => z
+  .number({
+    required_error: `${name} is required`,
+    invalid_type_error: `${name} must be a number`,
+  })
+  .int(
+    `${name} should be a positive integer`,
+  )
+  .positive(
+    `${name} should be a positive integer`,
   );
 
-  const knownInterestIsValid = InterestValidationSchema.safeParse(knownInterest).success;
-  const knownCompoundingPeriodsValueIsValid = CompoundingPeriodsValidationSchema.safeParse(knownCompoundingPeriods).success;
-  const desiredCompoundingPeriodsValueIsValid = CompoundingPeriodsValidationSchema.safeParse(desiredCompoundingPeriods).success;
-  const allValid = knownInterestIsValid && knownCompoundingPeriodsValueIsValid && desiredCompoundingPeriodsValueIsValid;
+const EquivalentInterestRateDataValidationSchema = z.object({
+  [knownInterestToken]: InterestValidationSchema,
+  [knownCompoundingPeriodsToken]: CompoundingPeriodsValidationSchema('The known compounding periods value'),
+  [desiredCompoundingPeriodsToken]: CompoundingPeriodsValidationSchema('The desired compounding periods value'),
+});
 
-  const desiredInterestToken = 'i_d';
-  const knownInterestToken = 'i_k';
-  const desiredCompoundingPeriodsToken = 'p_d';
-  const knownCompoundingPeriodsToken = 'p_k';
+type EquivalentInterestRateData = z.infer<typeof EquivalentInterestRateDataValidationSchema>;
 
-  const buildEquation = (
-    knownInterest: string | number,
-    knownCompoundingPeriods: string | number,
-    desiredCompoundingPeriods: string | number,
-  ) => {
-    return `(1 + ${knownInterest}) ^ (${knownCompoundingPeriods} / ${desiredCompoundingPeriods}) - 1`;
-  }
-
+const EquivalentInterestRatePageContent = () => {
   const tokenizedEquation = buildEquation(
     knownInterestToken,
     knownCompoundingPeriodsToken,
     desiredCompoundingPeriodsToken,
 
   );
-  const evaluatedEquation = buildEquation(
-    knownInterest,
-    knownCompoundingPeriods,
-    desiredCompoundingPeriods,
-
-  );
   const tokenizedComparison = `${desiredInterestToken} = ${tokenizedEquation}`
-  const evaluatedComparison = `${evaluatedEquation} = ${desiredInterestToken}`
 
   const equationTypography = {
     xs: 'h5',
@@ -110,105 +95,95 @@ const EquivalentInterestRatePageContent = () => {
     </InputAdornment>
   );
 
+  const initialValues: EquivalentInterestRateData = {
+    [knownInterestToken]: 0,
+    [knownCompoundingPeriodsToken]: 0,
+    [desiredCompoundingPeriodsToken]: 0,
+  };
+
   return (
-    <Stack
-      alignItems="center"
-      justifyContent="center"
-      spacing={2}
-      sx={{
-        my: {
-          xs: 2,
-          sm: 5,
-        },
-        textAlign: 'center',
-        color: 'textPrimary',
-        '* svg': {
-          color: 'textPrimary',
-          fill: 'currentColor',
-        },
-      }}
+    <Formik
+      initialValues={initialValues}
+      validate={
+        (values) => {
+          const errors: { [key: string]: (string | undefined) } = {};
+          const result = EquivalentInterestRateDataValidationSchema.safeParse(values);
+          if (!result.success) {
+            const issues = result.error.issues;
+            issues.forEach(issue => {
+              const field = issue.path.length > 0 ? issue.path[0] : undefined;
+              if (field) errors[field] = issue.message;
+            });
+          }
+          return errors;
+        }
+      }
+      onSubmit={() => { }}
     >
-      <Typography
-        sx={{
-          typography: equationTypography,
-        }}
-      >
-        <Equation
-          value={tokenizedComparison}
-        />
-      </Typography>
-      <Divider flexItem />
-      <ValueField
-        token={knownInterestToken}
-        value={knownInterest * 100}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => setKnownInterest(Math.max(0, e.target.valueAsNumber) / 100)}
-        InputProps={{
-          endAdornment: percentAdornment,
-        }}
-      />
-      <ValueField
-        token={desiredCompoundingPeriodsToken}
-        value={desiredCompoundingPeriods}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => setDesiredCompoundingPeriods(Math.max(0, e.target.valueAsNumber))}
-      />
-      <ValueField
-        token={knownCompoundingPeriodsToken}
-        value={knownCompoundingPeriods}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => setKnownCompoundingPeriods(Math.max(0, e.target.valueAsNumber))}
-      />
-      <Typography
-        sx={{
-          typography: equationTypography,
-          width: "100%",
-        }}
-      >
-        {
-          allValid
-            ?
+      {() => {
+        return (
+          <Form>
             <Stack
-              spacing={2}
               alignItems="center"
               justifyContent="center"
+              spacing={2}
+              sx={{
+                my: {
+                  xs: 2,
+                  sm: 5,
+                },
+                textAlign: 'center',
+                color: 'textPrimary',
+                '* svg': {
+                  color: 'textPrimary',
+                  fill: 'currentColor',
+                },
+              }}
             >
-              <EquationEvaluate
-                ref={equationRef}
-                value={evaluatedEquation}
-                decimals={{
-                  type: 'fixed',
-                  digits,
-                }}
-              />
-              <Divider flexItem />
-              <Card
-                variant="outlined"
+              <Typography
                 sx={{
-                  px: 3,
-                  py: 1,
+                  typography: equationTypography,
                 }}
               >
                 <Equation
-                  value={`${desiredInterestToken}=${Number((desiredInterest * 100).toFixed(digits - 2))}%`}
+                  value={tokenizedComparison}
                 />
-              </Card>
+              </Typography>
+              <ValueField
+                token={knownInterestToken}
+                InputProps={{
+                  endAdornment: percentAdornment,
+                }}
+              />
+              <ValueField
+                token={desiredCompoundingPeriodsToken}
+              />
+              <ValueField
+                token={knownCompoundingPeriodsToken}
+              />
+              <Box
+                sx={{
+                  pt: 5,
+                  typography: equationTypography,
+                  width: "100%",
+                }}
+              >
+                <Result />
+              </Box>
             </Stack>
-            :
-            <Equation
-              value={evaluatedComparison}
-            />
-        }
-      </Typography>
-    </Stack>
+          </Form>
+        );
+      }}
+    </Formik>
   );
 }
 
 interface ValueFieldProps {
   token: string;
-  value: number;
-  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
   InputProps?: Partial<FilledInputProps>;
 }
 
-const ValueField: FC<ValueFieldProps> = ({ token, value, onChange, InputProps }) => {
+const ValueField: FC<ValueFieldProps> = ({ token, InputProps }) => {
   return (
     <Stack
       direction="row"
@@ -222,19 +197,83 @@ const ValueField: FC<ValueFieldProps> = ({ token, value, onChange, InputProps })
         <Equation value={token} />
       </Typography>
       {' = '}
-      <TextField
-        disabled={!Boolean(onChange)}
-        value={value}
-        type="number"
-        onChange={onChange}
-        sx={{
-          ml: 1,
-          width: '15ch',
-        }}
-        size="small"
-        InputProps={InputProps}
-      />
+      <Field
+        id={token}
+        name={token}
+      >
+        {
+          ({ field, meta }: FieldProps) => (
+            <TextField
+              {...field}
+              error={Boolean(meta.touched && meta.error)}
+              type="number"
+              size="small"
+              InputProps={InputProps}
+              inputProps={{ min: 0 }}
+              sx={{
+                ml: 1,
+                width: '15ch',
+              }}
+            />
+          )
+        }
+      </Field>
     </Stack>
 
+  );
+}
+
+const Result: FC = () => {
+  const { isValid, values, dirty, errors } = useFormikContext<EquivalentInterestRateData>();
+  const equationRef = useRef<any>();
+  const [desiredInterest, setDesiredInterest] = useState<number>(0);
+  const evaluatedEquation = buildEquation(
+    (!dirty || errors[knownInterestToken]) ? knownInterestToken : (values[knownInterestToken] / 100),
+    (!dirty || errors[knownCompoundingPeriodsToken]) ? knownCompoundingPeriodsToken : values[knownCompoundingPeriodsToken],
+    (!dirty || errors[desiredCompoundingPeriodsToken]) ? desiredCompoundingPeriodsToken : values[desiredCompoundingPeriodsToken],
+  );
+  const evaluatedComparison = `${evaluatedEquation} = ${desiredInterestToken}`
+  useEffect(
+    () => {
+      setDesiredInterest(
+        equationRef.current == null
+          ? 0
+          : equationRef.current.result.value,
+      );
+    },
+  );
+  if (dirty && isValid) {
+    return (
+      <Stack
+        spacing={2}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <EquationEvaluate
+          ref={equationRef}
+          value={evaluatedEquation}
+          decimals={{
+            type: 'fixed',
+            digits,
+          }}
+        />
+        <Card
+          variant="outlined"
+          sx={{
+            px: 3,
+            py: 1,
+          }}
+        >
+          <Equation
+            value={`${desiredInterestToken}=${Number((desiredInterest * 100).toFixed(digits - 2))}%`}
+          />
+        </Card>
+      </Stack>
+    );
+  }
+  return (
+    <Equation
+      value={evaluatedComparison}
+    />
   );
 }
