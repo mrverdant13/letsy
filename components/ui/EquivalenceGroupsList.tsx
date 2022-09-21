18e1eb5fd689148a551793bd59555a202e9b1eb8
Karlo Verde <42245236+mrverdant13@users.noreply.github.com>
2022-09-21 00:16:48 -0500

    feat(eq-groups): show skeletons while loading (#36)

--- components/ui/EquivalenceGroupsList.tsx
+++ components/ui/EquivalenceGroupsList.tsx
@@ -1,36 +1,72 @@
 import { FC } from 'react';
 
-import { Grid } from "@mui/material";
+import { Box, Card, Grid, Skeleton } from "@mui/material";
 
 import { IPaymentGroup } from "../../interfaces/payment-group";
 import { EquivalenceGroupCard } from './EquivalenceGroupCard';
 
-interface Props {
-  groups: IPaymentGroup[];
+type Props =
+  | { type: 'loaded'; groups: IPaymentGroup[]; }
+  | { type: 'loading'; skeletonsCount: number; }
+  ;
+
+export const EquivalenceGroupsList: FC<Props> = (props) => {
+  const type = props.type;
+  return (
+    <Grid container spacing={2} px={5}>
+      {
+        type === 'loading' &&
+        Array.from(
+          { length: props.skeletonsCount },
+          (x, i) => (
+            <Grid
+              key={i}
+              item
+              xs={12}
+              sm={6}
+              md={4}
+            >
+              <Skeleton variant="rounded" width="100%" height={100} />
+            </Grid>
+          ),
+        )
+      }
+      {
+        type === 'loaded' &&
+        props.groups.map((group) => (
+          <GridItem
+            key={group._id}
+            group={group}
+          />
+        ))
+      }
+    </Grid >
+  )
 }
 
-export const EquivalenceGroupsList: FC<Props> = ({ groups }) => {
+interface GridItemProps {
+  group: IPaymentGroup;
+}
+
+const GridItem: FC<GridItemProps> = ({ group }) => {
   return (
-    <Grid container spacing={2} px={5} pb={8}>
-      {groups.map((group) => (
-        <Grid
-          key={group._id}
-          item
-          xs={12}
-          sm={6}
-          md={4}
-        >
-          <EquivalenceGroupCard
-            group={group}
-            sx={{
-              width: "100%",
-              height: "100%",
-              display: "flex",
-              alignItems: "center",
-            }}
-          />
-        </Grid>
-      ))}
+    <Grid
+      key={group._id}
+      item
+      xs={12}
+      sm={6}
+      md={4}
+    >
+      <EquivalenceGroupCard
+        group={group}
+        sx={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+        }}
+      />
     </Grid>
-  )
+
+  );
 }
