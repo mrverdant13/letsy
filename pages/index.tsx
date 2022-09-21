066448ef7533f17a1fe81559b121765f9d79fed5
Karlo Verde <42245236+mrverdant13@users.noreply.github.com>
2022-09-20 22:04:33 -0500

    feat: landing page (#34)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,62 @@
 import type { NextPage } from 'next'
 
-import { Typography } from '@mui/material';
+import { Typography, Box, Stack } from '@mui/material';
 
 import { BasePageLayout } from '../components/layouts';
+import Image from 'next/image';
 
 const HomePage: NextPage = () => {
   return (
-    <BasePageLayout>
-      <Typography
-        align="center"
-        variant="h1"
+    <BasePageLayout
+      sx={{
+        alignItems: 'start',
+        justifyContent: 'end',
+      }}
+    >
+      <Box
+        sx={{
+          position: 'fixed',
+          height: '100%',
+          width: '100%',
+          overflow: 'hidden',
+          zIndex: -1,
+        }}
       >
-        Welcome to Letsy!
-      </Typography>
+        <Image
+          src="/home-bg.jpg"
+          layout="fill"
+          objectFit="cover"
+        />
+      </Box>
+      <Stack
+        m={10}
+      >
+        <Typography
+          variant="h1"
+          sx={{
+            fontSize: {
+              xs: 60,
+              sm: 80,
+              md: 100,
+            },
+            fontWeight: 'bold',
+          }}
+        >
+          Letsy
+        </Typography>
+        <Typography
+          variant="h2"
+          sx={{
+            fontSize: {
+              xs: 24,
+              sm: 32,
+              md: 40,
+            },
+          }}
+        >
+          Tools for better decisions
+        </Typography>
+      </Stack>
     </BasePageLayout >
   )
 }
